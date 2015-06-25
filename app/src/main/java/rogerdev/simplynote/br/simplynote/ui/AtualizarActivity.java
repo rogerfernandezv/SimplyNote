98fd3d7a733964beff39822dddc154c3301eed8b
rogerfernandez <rogerfernandezv@gmail.com>
2015-06-24 23:17:05 -0300

    Melhorado as imagens agora suportando captura pela galeria de imagens.

--- app/src/main/java/rogerdev/simplynote/br/simplynote/ui/AtualizarActivity.java
+++ app/src/main/java/rogerdev/simplynote/br/simplynote/ui/AtualizarActivity.java
@@ -1,7 +1,14 @@
 package rogerdev.simplynote.br.simplynote.ui;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,6 +25,10 @@ import rogerdev.simplynote.br.simplynote.model.Anotacao;
 
 public class AtualizarActivity extends ActionBarActivity {
 
+    String picturePath = "";
+    ImageView imgview;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +36,7 @@ public class AtualizarActivity extends ActionBarActivity {
         Intent intent = getIntent();
         final Anotacao anotacaoAtualizar;
 
-        int idAnotacao = 0;
+        int idAnotacao;
 
         try {
             idAnotacao = Integer.parseInt(intent.getStringExtra(MainActivity.EXTRA_MESSAGE));
@@ -37,12 +48,26 @@ public class AtualizarActivity extends ActionBarActivity {
         Button btn_atualizar = (Button) findViewById(R.id.btn_atualizar);
         final EditText txtTitulo = (EditText) findViewById(R.id.txtTituloAtualizar);
         final EditText txtTexto = (EditText) findViewById(R.id.txtAnotacaoAtualizar);
-        final ImageView imgview = (ImageView) findViewById(R.id.imgNoteAtualizar);
+        imgview = (ImageView) findViewById(R.id.imgNoteAtualizar);
+
+        imgview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
 
         anotacaoAtualizar = AnotacaoDAO.getInstance().selectAnotacao(idAnotacao);
 
         txtTitulo.setText(anotacaoAtualizar.getTituloAnotacao());
         txtTexto.setText(anotacaoAtualizar.getTextoAnotacao());
+        picturePath = anotacaoAtualizar.getImg_uri();
+
+        if(picturePath.length()> 0) {
+            Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+            thumbnail = Bitmap.createScaledBitmap(thumbnail, 150, 150, true);
+            imgview.setImageBitmap(thumbnail);
+        }
 
 
 
@@ -51,7 +76,8 @@ public class AtualizarActivity extends ActionBarActivity {
             public void onClick(View v) {
                 anotacaoAtualizar.setTextoAnotacao(txtTexto.getText().toString());
                 anotacaoAtualizar.setTituloAnotacao(txtTitulo.getText().toString());
-                anotacaoAtualizar.setImg_uri(imgview.getTag().toString());
+                anotacaoAtualizar.setImg_uri(picturePath);
+
                 AnotacaoDAO.getInstance().atualizarAnotacao(anotacaoAtualizar);
                 finish();
             }
@@ -59,6 +85,47 @@ public class AtualizarActivity extends ActionBarActivity {
 
 
 
+    }
+
+    private void selectImage(){
+        final CharSequence[] options = {"Escolher imagem", "Cancelar"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(AtualizarActivity.this);
+        builder.setTitle("Adicionar imagem");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Escolher imagem")) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 1);
+                } else if (options[item].equals("Cancelar")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        builder.show();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            if(requestCode == 1)
+            {
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                thumbnail = Bitmap.createScaledBitmap(thumbnail, 150, 150, true);
+                imgview.setImageBitmap(thumbnail);
+            }
+        }
     }
 
     @Override
